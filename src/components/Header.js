e6738c63ecf2bfe56d83cdc0121004616835db56
ronald-it <ronald.cirgofer@hotmail.com>
2024-10-29 15:12:02 +0700

    Project colors added to tailwind config file. Header is finished.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,8 @@
 import CustomImage from "./CustomImage/CustomImage";
 
 export default function Header() {
-  return <header className="flex justify-around items-center py-4 border-2 border-black">
-    <h1>The Clueless Cook</h1>
+  return <header className="flex justify-around items-center py-4">
+    <h1 className="font-light text-darkblue">The Clueless Cook</h1>
     <CustomImage src='/images/Hamburger_icon.svg' alt='Hamburger icon' className='w-8' width={100} height={100} />
   </header>
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:
+      {
+        darkblue: {
+          DEFAULT: '#283E53'
+        },
+        lightblue: {
+          DEFAULT: '#70B6BE'
+        }
+      }
+    },
   },
   plugins: [],
 }
