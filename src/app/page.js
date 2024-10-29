f83535d4f66e41aa7e677608b247b2c19df229eb
ronald-it <ronald.cirgofer@hotmail.com>
2024-10-29 17:38:13 +0700

    Mobile design for popup modal is almost finished.

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,49 @@
+'use client';
+import Link from 'next/link';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import ReactModal from 'react-modal';
+import { useState } from 'react';
+import CloseIcon from '../components/CloseIcon';
 
 export default function Home() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const toggleModal = () => {
+    setIsModalOpen((prev) => !prev);
+  };
   return (
     <>
-      <Header />
+      <Header toggleModal={toggleModal} />
+      <ReactModal
+        isOpen={isModalOpen}
+        onRequestClose={toggleModal}
+        shouldCloseOnOverlayClick={true}
+        ariaHideApp={false}
+        className='h-full p-4'
+      >
+        <div className='h-full w-full flex justify-center items-center bg-darkblue rounded-xl [&>*]:text-white [&>*]:text-2xl [&>*]:font-light'>
+          <button className='absolute top-8 right-8 w-6' onClick={toggleModal}>
+            <CloseIcon />
+          </button>
+          <div className='flex flex-col w-3/4 justify-between items-center [&>*]:w-full [&>*]:flex [&>*]:justify-center [&>*]:mb-4 [&>*]:pb-4 [&>*]:border-b-2 [&>*]:border-white [&>*:last-child]:border-none [&>*:last-child]:m-0 [&>*:last-child]:p-0'>
+            <span>
+              <Link href='/'>Home</Link>
+            </span>
+            <span>
+              <Link href='/'>About</Link>
+            </span>
+            <span>
+              <Link href='/calculator'>Calculator</Link>
+            </span>
+            <span>
+              <Link href='/login'>Login</Link>
+            </span>
+            <span>
+              <Link href='/register'>Register</Link>
+            </span>
+          </div>
+        </div>
+      </ReactModal>
       <main></main>
       <Footer />
     </>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,18 @@
+import { useState } from 'react';
 import CustomImage from './CustomImage/CustomImage';
 
-export default function Header() {
+export default function Header({ toggleModal }) {
   return (
     <header className='flex justify-around items-center py-4'>
       <h1 className='font-light text-darkblue'>The Clueless Cook</h1>
-      <CustomImage
-        src='/images/Hamburger_icon.svg'
-        alt='Hamburger icon'
-        className='w-8'
-        width={100}
-        height={100}
-      />
+      <button className='w-8' onClick={toggleModal}>
+        <CustomImage
+          src='/images/Hamburger_icon.svg'
+          alt='Hamburger icon'
+          width={100}
+          height={100}
+        />
+      </button>
     </header>
   );
 }
