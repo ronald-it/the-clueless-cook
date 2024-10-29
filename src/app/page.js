2d35a24e84f48d773b3406801e0f4b843f1c5512
ronald-it <ronald.cirgofer@hotmail.com>
2024-10-29 15:38:49 +0700

    Footer finished.

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
+import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 export default function Home() {
   return (
     <>
     <Header/>
-  <main>
-  </main>
+    <main>
+
+    </main>
+    <Footer/>
   </>
   );
 }
