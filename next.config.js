86afc0d4f9a3939a154298893e17f0ce82cb7c7c
ronald-it <ronald.cirgofer@hotmail.com>
2025-03-09 13:10:49 +0100

    Changes made to Next Config file and CustomImage component for display of Images on new hosting platform.

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
   reactStrictMode: true,
   images: {
     unoptimized: true,
-  },
-  env: {
-    NEXT_PUBLIC_BASE_PATH: process.env.NODE_ENV === 'production' ? '/the-clueless-cook' : '',
   },
 };
 
--- src/components/CustomImage/CustomImage.js
+++ src/components/CustomImage/CustomImage.js
@@ -1,7 +1,5 @@
 import Image from 'next/image';
 
 export default function CustomImage({ src, alt, ...props }) {
-  const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
-  const fullPath = `${basePath}${src.startsWith('/') ? '' : '/'}${src}`;
-  return <Image {...props} src={fullPath} alt={alt} />;
+  return <Image {...props} src={src} alt={alt} />;
 }
