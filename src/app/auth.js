9821edeb36448fc95737e1bcb0302fd0fe7ed55a
ronald-it <ronald.cirgofer@hotmail.com>
2025-03-10 17:47:56 +0100

    Supabase connected to auth.js locally

--- src/app/auth.js
+++ src/app/auth.js
@@ -1,5 +1,10 @@
 import NextAuth from "next-auth"
+import { SupabaseAdapter } from "@auth/supabase-adapter"
  
-export const { handlers, signIn, signOut, auth } = NextAuth({
+export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [],
+  adapter: SupabaseAdapter({
+    url: process.env.SUPABASE_URL,
+    secret: process.env.SUPABASE_SERVICE_ROLE_KEY,
+  }),
 })
